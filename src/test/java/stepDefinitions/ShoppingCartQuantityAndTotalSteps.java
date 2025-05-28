fafecf0c6422116994b334ca4151f9bc625e4bf8
Damian Karalkevic <damian.karalkevic@gmail.com>
2025-05-28 13:46:19 +0300

    TC-407 implemented

--- src/test/java/stepDefinitions/ShoppingCartQuantityAndTotalSteps.java
+++ src/test/java/stepDefinitions/ShoppingCartQuantityAndTotalSteps.java
@@ -2,19 +2,26 @@ package stepDefinitions;
 
 import hooks.Hooks;
 import io.cucumber.java.en.*;
+import io.cucumber.java.sl.In;
 import org.openqa.selenium.By;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import static org.junit.Assert.*;
 
 public class ShoppingCartQuantityAndTotalSteps {
     private WebDriver driver;
     private WebDriverWait wait;
+
+    private final int testItemQuantity = 3; // Change test items quantity
     public ShoppingCartQuantityAndTotalSteps() {
         this.driver = Hooks.driver;
     }
@@ -28,7 +35,7 @@ public class ShoppingCartQuantityAndTotalSteps {
     public void productAddedToShoppingCartForTests() throws Exception{
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         driver.findElement(By.className("qty-input")).clear();
-        driver.findElement(By.className("qty-input")).sendKeys("3");
+        driver.findElement(By.className("qty-input")).sendKeys(Integer.toString(testItemQuantity));
         driver.findElement(By.className("add-to-cart-button")).click();
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("success")));
     }
@@ -38,10 +45,10 @@ public class ShoppingCartQuantityAndTotalSteps {
         driver.get("https://demowebshop.tricentis.com/cart");
     }
 
-    @When("^I input 0 as new quantity value$")
-    public void zeroAsItemQuantityInput(){
+    @When("^I input (\\d+) as new quantity value$")
+    public void insertItemQuantity(int number){
         driver.findElement(By.className("qty-input")).clear();
-        driver.findElement(By.className("qty-input")).sendKeys("0");
+        driver.findElement(By.className("qty-input")).sendKeys(Integer.toString(number));
     }
 
     @And("^I click update shopping cart button$")
@@ -55,8 +62,22 @@ public class ShoppingCartQuantityAndTotalSteps {
         assertTrue(driver.findElement(By.className("order-summary-content")).getText().equals("Your Shopping Cart is empty!"));
     }
 
-    @And("^Shopping cart link value is 0$")
-    public void linkValueWithEmptyCartTest(){
-        assertTrue(driver.findElement(By.className("cart-qty")).getText().equals("(0)"));
+    @And("^Shopping cart link value is (\\d+)$")
+    public void linkValueWithEmptyCartTest(int number){
+        assertTrue(driver.findElement(By.className("cart-qty")).getText().equals("("+number+")"));
+    }
+
+    @Then("^Quantity of item is bigger than previous and equals (\\d+)$")
+    public void checkNewQuantityBigger(int number){
+        assertTrue(testItemQuantity<number);
+        assertTrue(driver.findElement(By.className("qty-input")).getAttribute("value").equals(Integer.toString(number)));
+    }
+
+    @And("^Total price is calculated correctly$")
+    public void totalPriceCalculation(){
+        double unitPrice = Double.parseDouble(driver.findElement(By.className("product-unit-price")).getText());
+        int quantity = Integer.parseInt(driver.findElement(By.className("qty-input")).getAttribute("value"));
+        double actualTotal = Double.parseDouble(driver.findElement(By.cssSelector(".order-total strong")).getText());
+        assertEquals((unitPrice*quantity), actualTotal);
     }
 }
--- src/test/resources/shoppingCartPageQtyAndTotal.feature
+++ src/test/resources/shoppingCartPageQtyAndTotal.feature
@@ -4,6 +4,22 @@ Feature: Shopping cart item quantity functionality and total price calculations
     And Test item is added to shopping cart
     And I am on the shopping cart page
 
+  @TC-407
+  Scenario Outline: Increase item quantity
+    When I input <number> as new quantity value
+    And I click update shopping cart button
+    Then Quantity of item is bigger than previous and equals <number>
+    And Total price is calculated correctly
+    And Shopping cart link value is <number>
+
+    #Examples should be bigger than initial quantity of test item
+    Examples:
+    |number|
+    |5     |
+    |25    |
+    |7     |
+    |13    |
+
   @TC-409
   Scenario: Zero is inserted as item quantity
     When I input 0 as new quantity value
